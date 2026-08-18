7b3d50cce755d4c57e613bbe65a700c6f1b096cb
Cloyd Arvie G. Floremonte <s.cagfloremonte@usm.edu.ph>
2026-08-18 09:47:23 +0800

    Trust Render proxy to fix mixed content HTTPS scheme detection

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => CheckRole::class,
         ]);
+
+        $middleware->trustProxies(at: '*');
+        
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
